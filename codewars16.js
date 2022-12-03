d197728803c54925d908ba7d85910590b2e3b6ec
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-03 15:10:07 -0700

    Convert a Number to a String

---
+++ codewars16.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    return num.toString()
+  }
+
+  console.log(numberToString(67)) //'67'
+  console.log(numberToString(123)) //'123'
+  console.log(numberToString(-100)) //'-100'
